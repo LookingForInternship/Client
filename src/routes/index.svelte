1d468e0b53a714301b9e783d9bd9d402ebff3f1e
chaigqn <quentinchaignaudpro@gmail.com>
2021-12-02 19:13:52 +0100

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<div class="my-4">
+    <img class="h-14 px-16" src="/logo.png" alt="Logo">
+    <div class="flex items-center justify-center h-96 flex-col">
+        <h1 class="text-4xl sm:text-5xl font-black text-deeppurple">Retrouvez tous les naufrages de l'histoire gratuitement :</h1>
+        <input class="w-96 px-4 py-3 bg-red-700" type="text">
+    </div>
+</div>
